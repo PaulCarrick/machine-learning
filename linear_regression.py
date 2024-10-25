e1cca38a8ed3f3b062fe80774e232082fa01fcba
Paul Carrick <paulandvirginiacarrick@gmail.com>
2024-10-25 13:11:22 -0700

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -1,18 +1,18 @@
 # !/usr/local/bin/python3.12
+"""Machine learning linear regression test code."""
+# pylint: disable=unused-import
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import ssl
 import numpy as np
 import pandas as pd
-import ssl
 import matplotlib.pyplot as plt
 from IPython.display import clear_output
 from six.moves import urllib
 
-import tensorflow.compat.v2.feature_column as fc
-
 import tensorflow as tf
 
-ssl._create_default_https_context = ssl._create_unverified_context
+ssl._create_default_https_context = ssl._create_unverified_context   # pylint: disable=protected-access
 dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv')
 dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv')
 y_train = dftrain.pop('survived')
